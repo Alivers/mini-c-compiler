a251319b7bd8f05bf527fc23c30a98b2890fd35f
Alivers <321204342@qq.com>
2020-05-16 19:16:22 +0800

    remove something unnecessary

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/vsc-repo/homework/compiling
-# Build directory: /root/vsc-repo/homework/compiling/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
